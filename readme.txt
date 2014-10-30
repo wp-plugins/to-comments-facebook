87c66c7e4ba17c32eb016e491e6110a55946d95e
tihhgoncalves <tihhgoncalves@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-30 23:21:00 +0000

    Controle de Link Relacionado.
    git-svn-id: https://plugins.svn.wordpress.org/to-comments-facebook/trunk@1017059 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link:http://goo.gl/glsRg
 Tags: comments, facebook, comentários, hide
 Requires at least: 3.0
 Tested up to: 3.5
-Stable tag: 1.0.1
+Stable tag: 1.0.2
 
 Tira o sistema de comentário nativo do WordPress e adiciona o formato de comentários utilizando o Facebook.
 
@@ -23,6 +23,10 @@ Disponível nos **Idiomas**:
 2.  Ativando o plugin automaticamente o sistema nativo de comentários do WordPress é desativado e substituíto pelo sistema de Comentários do Facebook.
 3.  Para configurações, acesse: Configurações / To Comments Facebook
 == Changelog ==
+= 1.0.2 =
+
+*   Agora é possível configurar qual link deseja relacionar com os comentários. (shotlink ou permalink)
+
 = 1.0.1 =
 
 *   Corrigindo nome de arquivos que estavam gerando erro na instalação do plugin.
--- to-comments-facebook.php
+++ to-comments-facebook.php
@@ -4,7 +4,7 @@ Plugin Name: To Comments Facebook
 Plugin URI: 
 Description: Tira o sistema de comentário nativo do WordPress e adiciona o formato de comentários utilizando o Facebook.
 Author: Nova Brazil Agência Interativa
-Version: 1.0.1
+Version: 1.0.2
 Author URI: http://www.novabrazil.art.br
 */
 
@@ -20,6 +20,7 @@ function ToCommentsFacebook_install(){
   $reg['comments_width']      = 500;
   $reg['comments_beforeHTML'] = __('<h3>Coment&aacute;rios no Facebook</h3>', 'tcf');
   $reg['comments_afterHTML']  = '';
+  $reg['comments_link']  = 'shotlink';
   update_option('tcf_config', $reg);
 }
 
@@ -33,13 +34,13 @@ $reg = get_option('tcf_config');
 add_action('admin_menu', 'ToCommentsFacebook_config_page');
 
 function ToCommentsFacebook_config_page() {
-  add_submenu_page('options-general.php', 
-	                 'To Comments Facebook',
-	                 'To Comments Facebook', 
-	                 'manage_options', 
-	                 'ToCommentsFacebook', 
-	                 'ToCommentsFacebook_conf'
-	                 );
+  add_submenu_page('options-general.php',
+      'To Comments Facebook',
+      'To Comments Facebook',
+      'manage_options',
+      'ToCommentsFacebook',
+      'ToCommentsFacebook_conf'
+  );
 }
 
 
@@ -53,75 +54,95 @@ if( (!empty($_POST['comments_width'])) || (!empty($_POST['comments_beforeHTML'])
   $reg['comments_width'] = $_POST['comments_width'];
   $reg['comments_beforeHTML'] = $_POST['comments_beforeHTML'];
   $reg['comments_afterHTML'] = $_POST['comments_afterHTML'];
-  
+  $reg['comments_link']  = $_POST['comments_link'];
+
   update_option('tcf_config', $reg);
   $msg = __('Cadastro atualizado com êxito.', 'tcf');
 
 }
 function ToCommentsFacebook_conf() {
   global $reg, $msg;
-?>
 
-<div class="wrap">
-  <img src="<?= plugins_url( 'nbrazil.png', __FILE__ ); ?>" width="36" height="36" style="float:left; margin:7px">
-  <h2>To Comments Facebook</h2>
+  //se já for um plugin antigo, não terá o parametro do LINK
 
-  <?
-  //mensagem
-  if(!empty($msg)){
-    echo('<div id="setting-error-settings_updated" class="updated settings-error">' . "\n");
-    echo('<p>' . "\n");
-    echo('<strong>' . $msg . '</strong>' . "\n");
-    echo('</p>' . "\n");
-    echo('</div>' . "\n");
+  if(!isset($reg['comments_link'])){
+    $reg['comments_link'] = 'permalink';
   }
-  ?>  
-  <form action="" method="post" id="">
-    <p><?= __('Preencha abaixo a configuração necessária para o funcionamento do Comentário do Facebook.', 'tcf'); ?></p>
-    
-    
-<table class="form-table">
-  <tbody>
-    <tr valign="top">
-      <th scope="row">
-        <label for="blogname"><b><?= __('Largura do Bloco', 'tcf'); ?></b></label>
-      </th>
-      <td>
-        <input id="comments_width" class="small-text" type="text" value="<?= $reg['comments_width']; ?>" name="comments_width"> pixels
-      </td>
-    </tr>
+  ?>
 
-    <tr valign="top">
-      <th scope="row">
-        <label for="blogname"><b><?= __('Código HTML exibido ANTES', 'tcf'); ?></b></label>
-        <p><?= __('Digite o código HTML que será exibido antes do Bloco de Comentário do Facebook.', 'tcf'); ?></p>
-      </th>
-      <td>
-        <textarea id="comments_beforeHTML" class="large-text code" rows="3" name="comments_beforeHTML"><?= $reg['comments_beforeHTML']; ?></textarea>
-      </td>
-    </tr>   
-    
-    <tr valign="top">
-      <th scope="row">
-        <label for="blogname"><b><?= __('Código HTML exibido DEPOIS', 'tcf'); ?></b></label>
-        <p><?= __('Digite o código HTML que será exibido depois do Bloco de Comentário do Facebook.', 'tcf'); ?></p>
-      </th>
-      <td>
-        <textarea id="comments_afterHTML" class="large-text code" rows="3" name="comments_afterHTML"><?= $reg['comments_afterHTML']; ?></textarea>
-      </td>
-    </tr>   
-     
-  </tbody>
-</table>
+  <div class="wrap">
+    <img src="<?= plugins_url( 'nbrazil.png', __FILE__ ); ?>" width="36" height="36" style="float:left; margin:7px">
+    <h2>To Comments Facebook</h2>
 
-    <p class="submit">
-     <input id="submit" class="button button-primary" type="submit" value="<?= __('Salvar alterações', 'tcf'); ?>" name="submit">
-    </p>
-  </form>
-  
-  <div><?= __('Este plugin é desenvolvido por', 'tcf'); ?> <a href="http://www.novabrazil.art.br" target="_blank" title="<?= __('Nova Brazil Agência Interativa', 'tcf'); ?>"><?= __('Nova Brazil Agência Interativa', 'tcf'); ?></a>.</div>
-   
-</div>
+    <?
+    //mensagem
+    if(!empty($msg)){
+      echo('<div id="setting-error-settings_updated" class="updated settings-error">' . "\n");
+      echo('<p>' . "\n");
+      echo('<strong>' . $msg . '</strong>' . "\n");
+      echo('</p>' . "\n");
+      echo('</div>' . "\n");
+    }
+    ?>
+    <form action="" method="post" id="">
+      <p><?= __('Preencha abaixo a configuração necessária para o funcionamento do Comentário do Facebook.', 'tcf'); ?></p>
+
+
+      <table class="form-table">
+        <tbody>
+        <tr valign="top">
+          <th scope="row">
+            <label for="blogname"><b><?= __('Largura do Bloco', 'tcf'); ?></b></label>
+          </th>
+          <td>
+            <input id="comments_width" class="small-text" type="text" value="<?= $reg['comments_width']; ?>" name="comments_width"> pixels
+          </td>
+        </tr>
+
+        <tr valign="top">
+          <th scope="row">
+            <label for="blogname"><b><?= __('Código HTML exibido ANTES', 'tcf'); ?></b></label>
+            <p><?= __('Digite o código HTML que será exibido antes do Bloco de Comentário do Facebook.', 'tcf'); ?></p>
+          </th>
+          <td>
+            <textarea id="comments_beforeHTML" class="large-text code" rows="3" name="comments_beforeHTML"><?= $reg['comments_beforeHTML']; ?></textarea>
+          </td>
+        </tr>
+
+        <tr valign="top">
+          <th scope="row">
+            <label for="blogname"><b><?= __('Código HTML exibido DEPOIS', 'tcf'); ?></b></label>
+            <p><?= __('Digite o código HTML que será exibido depois do Bloco de Comentário do Facebook.', 'tcf'); ?></p>
+          </th>
+          <td>
+            <textarea id="comments_afterHTML" class="large-text code" rows="3" name="comments_afterHTML"><?= $reg['comments_afterHTML']; ?></textarea>
+          </td>
+        </tr>
+
+        <tr valign="top">
+          <th scope="row">
+            <label for="blogname"><b><?= __('Relacionar link', 'tcf'); ?></b></label>
+            <p><?= __('Recomendamos utilizar <u>shotlink</u>.', 'tcf'); ?></p>
+          </th>
+          <td>
+            <select name="comments_link">
+              <option value="shotlink"  <?= ($reg['comments_link'] == 'shotlink'?'selected':null);?>>shotlink (Ex.: http://www.meublog.com.br/?p=123) </option>
+              <option value="permalink" <?= ($reg['comments_link'] == 'permalink'?'selected':null);?>>permalink (Ex.: http://www.meublog.com.br/2014/12/titulo-do-post) </option>
+            </select>
+          </td>
+        </tr>
+
+        </tbody>
+      </table>
+
+      <p class="submit">
+        <input id="submit" class="button button-primary" type="submit" value="<?= __('Salvar alterações', 'tcf'); ?>" name="submit">
+      </p>
+    </form>
+
+    <div><?= __('Este plugin é desenvolvido por', 'tcf'); ?> <a href="http://www.novabrazil.art.br" target="_blank" title="<?= __('Nova Brazil Agência Interativa', 'tcf'); ?>"><?= __('Nova Brazil Agência Interativa', 'tcf'); ?></a>.</div>
+
+  </div>
 <?php
 }
 
